9e519e1d09f640eafdd0b5033e245c2190e8f243
Anurodh01 <67429000+Anurodh01@users.noreply.github.com>
2022-06-26 11:14:07 +0530

    map removed

--- js/map.js
+++
@@ -1,8 +0,0 @@
-function mymap() {
-    var mapcanvas = document.getElementById("map")
-    var mapoptions = {
-        center: new google.maps.LatLng(26.7922, 82.1998),
-        zoom: 10
-    }
-    var map = new google.maps.Map(mapcanvas, mapoptions)
-}
