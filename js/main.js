1e27e9e9de1066d7154316ef4897e2e6478f1df9
Anurodh01 <67429000+Anurodh01@users.noreply.github.com>
2022-06-26 10:51:54 +0530

    map change added

--- js/main.js
+++ js/main.js
@@ -289,12 +289,3 @@
     });
 
 })(jQuery);
-
-function mymap() {
-    var mapcanvas = document.getElementById("map")
-    var mapoptions = {
-        center: new google.maps.LatLng(26.7922, 82.1998),
-        zoom: 10
-    }
-    var map = new google.maps.Map(mapcanvas, mapoptions)
-}
